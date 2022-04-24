9250dfb1bff45d06a02040e3da4d3a522d541a43
FantasyerOuO <75434717+FantasyerOuO@users.noreply.github.com>
2022-04-24 21:25:17 +0800

    Features Good

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import tkinter as tk
 
 def btn1():
-    cm=ent1.get()
-    kg=ent2.get()
-    float(cm)
-    float(kg)
+    def judgement(bmi):
+        if bmi>=18.5 and bmi<24:
+            return True
+        else:
+            return False
+    
+    cm=float(ent1.get())
+    kg=float(ent2.get())
     bmi=(kg)/((cm)/100)**2
-    print(bmi)
+    J=judgement(bmi)
+    lab4.config(text=round(bmi,2))
+    lab5.config(tk.PhotoImage(file=''))
     
 
 win=tk.Tk()
-win.geometry('200x400')
+win.geometry('400x200')
 win.resizable(False,False)
 win.config(bg='cyan')
-win.title='BMI Calculator by Hui'
+win.title('BMI Calculator by Hui')
 
 lab0=tk.Label(text='BMI計算器')
 lab0.config(bg='pink',font=('微軟正黑體',30,'bold'))
@@ -31,12 +37,16 @@ lab2.grid(row=2,column=0)
 ent2=tk.Entry()
 ent2.grid(row=2,column=1)
 
-btn1=tk.Button(text='開始計算',command=btn1())
-btn1.config()
+btn1=tk.Button(text='開始計算',command=btn1)
+
 btn1.grid(row=3,columnspan=2)
 
 lab4=tk.Label()
 lab4.config(bg='cyan')
-lab4.grid(row=4,columnspan=2)
+lab4.grid(row=0,column=2,rowspan=2,columnspan=2)
+
+lab5=tk.Label()
+lab4.config(bg='cyan')
+lab4.grid(row=2,column=2,rowspan=2,columnspan=2)
 
 win.mainloop()
